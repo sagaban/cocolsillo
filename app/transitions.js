2740bb50fef9bfecd0fb877021c8e7ec502bd908
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2016-12-06 14:09:00 +0100

    Change some buttons an add animations

--- app/transitions.js
+++ app/transitions.js
@@ -1,7 +1,27 @@
 export default function() {
+
   this.transition(
     this.fromRoute('index'),
     this.toRoute('register'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  // Login
+  this.transition(
+    this.fromRoute('index'),
+    this.toRoute('main'),
+    this.use('fade')
+  );
+  this.transition(
+    this.hasClass('navbar'),
+    this.toValue(true),
+    this.use('wait', 1500, { then: 'toDown' }),
+    this.reverse('toUp')
+  );
+  this.transition(
+    this.fromRoute('main.index'),
+    this.toRoute('main.account'),
     this.use('toLeft'),
     this.reverse('toRight')
   );
